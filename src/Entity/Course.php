e29805e4688a2a457f2e37381fd953fb8c46c155
husseinhareb <shtam.hareb12@gmail.com>
2025-04-05 23:54:53 +0200

    Created relation between Course and User

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -3,10 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\CourseRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CourseRepository::class)]
 class Course
@@ -15,111 +15,144 @@ class Course
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    
     #[ORM\Column(length: 255)]
     private ?string $title = null;
-
+    
     #[ORM\Column(length: 255)]
     private ?string $content = null;
-
+    
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
-
+    
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
-
+    
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt = null;
-
+    
     #[ORM\Column(type: "string", length: 255, nullable: true)]
     private ?string $imagePath = null;
-
-    private ?File $image = null; 
-
+    
+    private ?File $image = null;
+    
+    #[ORM\OneToMany(mappedBy: 'course', targetEntity: Enrollment::class, cascade: ['persist', 'remove'])]
+    private Collection $enrollments;
+    
+    public function __construct()
+    {
+        $this->enrollments = new ArrayCollection();
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
     }
-
+    
     public function getTitle(): ?string
     {
         return $this->title;
     }
-
-    public function setTitle(string $title): static
+    
+    public function setTitle(string $title): self
     {
         $this->title = $title;
         return $this;
     }
-
+    
     public function getContent(): ?string
     {
         return $this->content;
     }
-
-    public function setContent(string $content): static
+    
+    public function setContent(string $content): self
     {
         $this->content = $content;
         return $this;
     }
-
+    
     public function getSlug(): ?string
     {
         return $this->slug;
     }
-
-    public function setSlug(string $slug): static
+    
+    public function setSlug(string $slug): self
     {
         $this->slug = $slug;
         return $this;
     }
-
+    
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
     }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
     {
         $this->createdAt = $createdAt;
         return $this;
     }
-
+    
     public function getUpdatedAt(): ?\DateTimeImmutable
     {
         return $this->updatedAt;
     }
-
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
         return $this;
     }
-
-    public function getImage(): ?File
-    {
-        return $this->image;
-    }
-
-    public function setImage(?File $image = null): static
-    {
-        $this->image = $image;
-
-        if ($image) {
-            $this->updatedAt = new \DateTimeImmutable();
-        }
-
-        return $this;
-    }
-
+    
     public function getImagePath(): ?string
     {
         return $this->imagePath;
     }
-
-    public function setImagePath(?string $imagePath): static
+    
+    public function setImagePath(?string $imagePath): self
     {
         $this->imagePath = $imagePath;
+        return $this;
+    }
+    
+    public function getImage(): ?File
+    {
+        return $this->image;
+    }
+    
+    public function setImage(?File $image = null): self
+    {
+        $this->image = $image;
+        if ($image) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+        return $this;
+    }
+    
+    /**
+     * @return Collection|Enrollment[]
+     */
+    public function getEnrollments(): Collection
+    {
+        return $this->enrollments;
+    }
+    
+    public function addEnrollment(Enrollment $enrollment): self
+    {
+        if (!$this->enrollments->contains($enrollment)) {
+            $this->enrollments[] = $enrollment;
+            $enrollment->setCourse($this);
+        }
+        return $this;
+    }
+    
+    public function removeEnrollment(Enrollment $enrollment): self
+    {
+        if ($this->enrollments->removeElement($enrollment)) {
+            if ($enrollment->getCourse() === $this) {
+                $enrollment->setCourse(null);
+            }
+        }
         return $this;
     }
 }
